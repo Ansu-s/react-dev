9246c6b9512629ae17d4cca86997cfd0dfda4e00
Ansu-s <130679461+Ansu-s@users.noreply.github.com>
2024-04-07 18:24:35 +0530

    adding basic bootstrtap

--- react-dev/src/App.js
+++ react-dev/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
+import Spinner from 'react-bootstrap/Spinner';
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Nav variant="pills" defaultActiveKey="/home">
+        <Nav.Item>
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/user">Users</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href="/org">Organisation</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">About</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>
+            About
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+      
   );
 }
-
 export default App;
