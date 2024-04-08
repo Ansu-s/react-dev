22f4734f66cf53562971565700a24d1193ce6493
Ansu-s <csahoo222@gmail.com>
2024-04-08 15:57:14 +0530

    props and state

--- README.md
+++ README.md
@@ -6,3 +6,9 @@
 
 ![image](https://github.com/Ansu-s/react-dev/assets/130679461/6eec5bab-9b91-4d9d-9d23-9cd12cd41119)
 
+## Day: 2 (Props and State)  
+![image](https://github.com/Ansu-s/react-dev/assets/130679461/73607f48-f88a-4e2d-9305-e6b0baae5051)
+
+![image](https://github.com/Ansu-s/react-dev/assets/130679461/c7058350-b811-424b-ace3-2d3b8d3ce173)
+
+
--- react-dev/src/components/TextiInputForm.js
+++ react-dev/src/components/TextiInputForm.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
+
 export default function TextiInputForm() {
+  const [text, updateText] = useState("Hello");
+  // const updateTextHandler = ()=>{
+  //   updateText("Greetings");
+  // };
+ const clickHandler = ()=>{
+console.log("You have clicked on the button" + text);
+let uppercaseText = text.toUpperCase();
+updateText(uppercaseText)
+};
+const onchangeValue = (event)=> {
+  console.log("It is changed")
+  updateText(event.target.value)
+
+ };
   return (
-    <>
+    <> 
+    
   <Form>
+    {/* <h1>{text} User</h1> */}
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Enter your text</Form.Label>
-        <Form.Control type="email" placeholder="your text input goes here" />
-        <Form.Text className="text-muted">
+        <Form.Control type="text"  value= {text} onChange={onchangeValue} placeholder="your text input goes here" />
+        <Form.Text  clas sName="text-muted">
           Whatever you enter will be seen here
         </Form.Text>
       </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
+      <Button variant="primary" type="button" onClick={clickHandler}>
+        Convert to Uppercase
       </Button>
     </Form>
   </>
   );
+}
+
+export function greet(props) {
+  return <h1>Hello {props.name}</h1>;
 }
